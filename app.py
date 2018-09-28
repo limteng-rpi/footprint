cd261308e011a714484ad71d39d0efb85faaf985
limteng-rpi <linying@limteng.com>
2018-09-28 19:16:09 -0400

    api

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask
+import os
+import logging
+from flask import Flask, request, jsonify
+from argparse import ArgumentParser
+from database import Database, Project, Task, Config, Result
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+cur_dir = os.path.dirname(__file__)
+
+parser = ArgumentParser()
+parser.add_argument('--dbpath', default=os.path.join(cur_dir, 'database'),
+                    help='Path to the database')
+args = parser.parse_args()
+
+db = Database(args.dbpath)
 
 app = Flask(__name__)
 
+# db.create_project('proj1')
+# db.get_project('proj1').create_task('task1')
+# db.get_project('proj1').get_task('task1').create_subtask('subtask1')
 
 @app.route('/')
 def home():
     pass
 
+@app.route('/settings')
+def setting():
+    pass
+
+
+@app.route('/api/<op>', methods=['GET', 'POST'])
+def api(op):
+    args = request.values
+    print(args.to_dict())
+    success, msg = db.api(op, args)
+    return jsonify({'msg': msg}), 200 if success else 500
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,283 +1,35 @@
 import os
+import re
 import json
 import time
 import shutil
+import logging
+import traceback
+
+logger = logging.getLogger()
 
 root = os.path.dirname(os.path.abspath(__file__))
-db_dir_path = os.path.join(root, 'database')
-db_json_path = os.path.join(db_dir_path, 'database.json')
-proj_dir_path = os.path.join(db_dir_path, 'projects')
-ROOT = os.path.dirname(os.path.abspath(__file__))
+# db_dir_path = os.path.join(root, 'database')
+# db_json_path = os.path.join(db_dir_path, 'database.json')
+# proj_dir_path = os.path.join(db_dir_path, 'projects')
+# ROOT = os.path.dirname(os.path.abspath(__file__))
 DEFAULT_DB_PATH = os.path.join(root, 'database')
 
 
-
-def append_database_json_val(k: str, v: str) -> bool:
-    """
-    :param k: Property key.
-    :param v: Value to append.
-    :return:
-    """
-    try:
-        db_json = json.load(open(db_json_path, 'r', encoding='utf-8'))
-        if k in db_json:
-            if v not in db_json[k]:
-                db_json[k].append(v)
-        else:
-            db_json[k] = [v]
-        json.dump(db_json, open(db_json_path, 'w', encoding='utf-8'))
-        return True
-    except:
-        return False
+def validate_name(name: str) -> bool:
+    return re.match('^[0-9a-zA-Z_-]{2,50}$', name) is not None
 
 
-def remove_database_json_val(k: str, v: str) -> bool:
-    """
-    :param k: Property key.
-    :param v: Value to remove.
-    :return:
-    """
-    try:
-        db_json = json.load(open(db_json_path, 'r', encoding='utf-8'))
-        if k in db_json and v in db_json[k]:
-            db_json[k].remove(v)
-            json.dump(db_json, open(db_json_path, 'w', encoding='utf-8'))
-            return True
-    except:
-        return False
-    return False
-
-#
-# Project operations
-#
-
-def project_exist(name: str) -> bool:
-    """Check if a project exists by name.
-    :param name: Project name.
-    :return:
-    """
-    proj_path = os.path.join(db_dir_path, 'projects', name)
-    return (os.path.exists(proj_path) and os.path.isdir(proj_path))
-
-
-def create_project(name: str, desc: str = '') -> (bool, str):
-    """Create a project.
-
-    :param name: Project name.
-    :param desc: Description.
-    :return:
-    """
-    create_time = int(time.time())
-
-    if project_exist(name):
-        return False, 'Project exists'
-    else:
-        try:
-            # Create project directory
-            proj_path = os.path.join(proj_dir_path, name)
-            os.makedirs(proj_path)
-            # Create properties.json
-            json.dump({'name': name,
-                       'description': desc,
-                       'tasks': [],
-                       'create': create_time
-            }, open(os.path.join(proj_path, 'properties.json'),
-                    'w', encoding='utf-8'))
-            # Add to database.json
-            append_database_json_val('projects', name)
-            return True,
-        except Exception as e:
-            return False, str(e)
-
-
-def delete_project(name) -> (bool, str):
-    """Delete a project by name.
-    :param name: Project name.
-    :return:
-    """
-    if project_exist(name):
-        try:
-            shutil.rmtree(os.path.join(proj_dir_path, name))
-            remove_database_json_val('projects', name)
-            return True,
-        except:
-            return False, 'Failed to delete the project'
-    else:
-        return False, 'Project doesn\'t exist'
-
-
-def modify_project_desc(name: str, desc: str) -> (bool, str):
-    pass
-
-
-def get_project_info(name: str) -> dict:
-    if project_exist(name):
-        proj_json = json.load(
-            open(os.path.join(proj_dir_path, name, 'properties.json'),
-                 'r', encoding='utf-8'))
-        return proj_json
-    else:
-        return {}
-
-
-def get_project_list(info: bool = True) -> list:
-    db_json = json.load(open(db_json_path, 'r', encoding='utf-8'))
-    if info:
-        projs = []
-        for proj in db_json['projects']:
-            info = get_project_info(proj)
-            if info:
-                projs.append(info)
-        return projs
-    else:
-        return db_json['projects']
-
-
-#
-# Task operations
-#
-
-def create_task(task_name: str, proj_name: str,
-                desc: str = '', conf: str = '',
-                tag: str = '') -> (bool, str):
-    pass
-
-
-def delete_task(task_name: str, proj_name: str) -> (bool, str):
-    pass
-
-
-def modify_task_desc(task_name: str, proj_name: str, desc: str) -> (bool, str):
-    pass
-
-
-def set_task_status(task_name: str, proj_name: str, status: str) -> (bool, str):
-    pass
-
-
-def modify_task_tags(task_name: str, proj_name: str, tags: str) -> (bool, str):
-    pass
-
-
-def modify_task_conf(task_name: str, proj_name: str, conf: str) -> (bool, str):
-    pass
-
-
-#
-# Result operations
-#
-RESULT_APPEND_FUNC = {}
-
-
-def add_result(proj_name: str, task_name: str, rst_name: str, rst_type: str
-               ) -> (bool, str):
-    pass
-
-
-def remove_result(proj_name: str, task_name: str, rst_name: str) -> (bool, str):
-    pass
-
-
-def rst_append_func(func):
-    """Decorator function to register result appending functions.
-    :param func: Result appending function.
-    """
-    def register_func(name):
-        RESULT_APPEND_FUNC[name] = func
-    return register_func
-
-
-def append_result_val(proj_name: str, task_name: str, rst_name: str, rst_type: str,
-                      val: list) -> (bool, str):
-    """
-    :param proj_name: Project name.
-    :param task_name: Task name.
-    :param rst_name: Result name.
-    :param rst_type: Result type.
-    :param val: A list of values.
-    :return:
-    """
-    if rst_type in RESULT_APPEND_FUNC:
-        return RESULT_APPEND_FUNC[rst_type](proj_name, task_name, rst_name, val)
-    else:
-        return False, 'Unknown result type'
-
-
-@rst_append_func('table')
-def append_table_val(proj_name: str, task_name: str, rst_name: str, val: list
-                  ) -> (bool, str):
-    """
-    :param proj_name: Project name.
-    :param task_name: Task name.
-    :param rst_name: Result name.
-    :param val: A list of values.
-    :return:
-    """
-    pass
-
-
-@rst_append_func('2dplot')
-def append_2dplot_val(proj_name: str, task_name: str, rst_name: str, val: list
-                   ) -> (bool, str):
-    """
-    :param proj_name: Project name.
-    :param task_name: Task name.
-    :param rst_name: Result name.
-    :param val: A list of values.
-    :return:
-    """
-    pass
-
-
-class Project(object):
-    def __init__(self,
-                 name: str,
-                 desc: str = '',
-                 tasks: list = None,
-                 archived: bool = False,
-                 create_time: int = -1,
-                 db = None
-                 ):
-        self.name = name
-        self.desc = desc
-        self.tasks = tasks if tasks else []
-        self.archived = archived
-        self.create_time = int(time.time()) if create_time == -1 else create_time
+class Container(object):
+    def __init__(self):
         self.metadata = {}
-        self.db = db
-
-    def has_task(self, name: str) -> bool:
-        pass
-
-    def create(self):
-        assert self.db is not None
-        proj_path = os.path.join(self.db.proj_dir_path, self.name)
-        metadata_path = os.path.join(proj_path, 'metadata.json')
-        # Make project directory
-        os.mkdir(proj_path)
-        # Create metadata.json
-        metadata = {
-            'name': self.name,
-            'desc': self.desc,
-            'tasks': self.tasks,
-            'archived': self.archived,
-            'create_time': self.create_time
-        }
-        json.dump(metadata, open(metadata_path, 'w', encoding='utf-8'))
-
-
-class Database(object):
-    def __init__(self, path: str = DEFAULT_DB_PATH):
-        self.path = path
-        self.proj_dir_path = os.path.join(path, 'projects')
-        self.db_metadata_path = os.path.join(path, 'metadata.json')
-        self.metadata = self.read_metadata()
+        self.metadata_path = None
 
     def read_metadata(self):
-        return json.load(open(self.db_metadata_path, 'r', encoding='utf-8'))
+        return json.load(open(self.metadata_path, 'r', encoding='utf-8'))
 
     def save_metadata(self):
-        json.dump(self.metadata, open(self.db_metadata_path, 'w', encoding='utf-8'))
+        json.dump(self.metadata, open(self.metadata_path, 'w', encoding='utf-8'))
 
     def add_metadata(self, key: str, val):
         """Add a key-value pair to the metadata.
@@ -325,24 +77,194 @@ class Database(object):
         else:
             raise KeyError('{} does not exist')
 
+
+class Config(object):
+
+    def __init__(self):
+        pass
+
+
+class Result(object):
+
+    def __init__(self, name: str):
+        pass
+
+
+class Task(Container):
+
+    def __init__(self,
+                 name: str,
+                 metadata: dict = None,
+                 path: str = ''
+                 ):
+        super().__init__()
+        self.name = name
+        self.path = path
+        self.metadata_path = os.path.join(path,
+                                          'metadata.json') if path else None
+        if metadata is None:
+            if os.path.exists(self.metadata_path):
+                self.metadata = self.read_metadata()
+            else:
+                self.metadata = {}
+        else:
+            self.metadata = metadata
+
+
+    def has_subtask(self, name: str) -> bool:
+        return name in self.metadata.get('subtasks', [])
+
+    def create(self):
+        os.mkdir(self.path)
+        os.mkdir(os.path.join(self.path, 'subtasks'))
+        self.save_metadata()
+
+    def create_subtask(self, name: str):
+        if self.has_subtask(name):
+            raise ValueError('Subtask {} already exists')
+        if not validate_name(name):
+            raise ValueError('Invalid task name')
+
+        task = Task(name, path=os.path.join(self.path, 'subtasks', name))
+        task.create()
+        task.add_metadata('name', name)
+        task.add_metadata('identifier', '{}/{}'.format(
+            self.metadata['identifier'], name))
+        self.append_metadata_item('subtasks', name)
+
+    def delete_subtask(self, name: str):
+        if self.has_subtask(name):
+            shutil.rmtree(os.path.join(self.path, 'subtasks', name))
+            self.remove_metadata_item('subtasks', name)
+        else:
+            raise ValueError('Subtask {} does not exists')
+
+    def create_child(self, name: str):
+        return self.create_subtask(name)
+
+    def delete_child(self, name):
+        return self.delete_child(name)
+
+    def has_child(self, name):
+        return self.has_subtask(name)
+
+
+class Project(Container):
+    def __init__(self,
+                 name: str,
+                 metadata: dict = None,
+                 path: str = '',
+                 ):
+        super().__init__()
+
+        self.name = name
+        self.path = path
+        self.metadata_path = os.path.join(path, 'metadata.json') if path else None
+        if metadata is None:
+            if os.path.exists(self.metadata_path):
+                self.metadata = self.read_metadata()
+            else:
+                self.metadata = {}
+        else:
+            self.metadata = metadata
+
+    def has_task(self, name: str) -> bool:
+        return name in self.metadata.get('tasks', [])
+
+    def create(self):
+        os.mkdir(self.path)
+        os.mkdir(os.path.join(self.path, 'tasks'))
+        self.save_metadata()
+
+    def create_task(self, name: str):
+        if self.has_task(name):
+            raise ValueError('Task {} already exists')
+        if not validate_name(name):
+            raise ValueError('Invalid task name')
+
+        task = Task(name, path=os.path.join(self.path, 'tasks', name))
+        task.create()
+        task.add_metadata('name', name)
+        task.add_metadata('identifier', '{}/{}'.format(
+            self.metadata['identifier'], name))
+        self.append_metadata_item('tasks', name)
+
+    def delete_task(self, name: str):
+        if self.has_task(name):
+            shutil.rmtree(os.path.join(self.path, 'tasks', name))
+            self.remove_metadata_item('tasks', name)
+        else:
+            raise ValueError('Task {} does not exists')
+
+    def get_task(self, name: str):
+        if self.has_task(name):
+            return Task(name, path=os.path.join(self.path, 'tasks', name))
+        else:
+            raise ValueError('Task {} does not exists'.format(name))
+
+    def has_child(self, name: str):
+        return self.has_task(name)
+
+    def create_child(self, name: str):
+        return self.create_task(name)
+
+    def delete_child(self, name: str):
+        return self.delete_child(name)
+
+    def get_child(self, name: str):
+        return self.get_task(name)
+
+
+class Database(Container):
+    def __init__(self, path: str = DEFAULT_DB_PATH):
+        super().__init__()
+
+        self.path = path
+        self.proj_dir_path = os.path.join(path, 'projects')
+        self.metadata_path = os.path.join(path, 'metadata.json')
+        if os.path.exists(self.metadata_path):
+            self.metadata = self.read_metadata()
+        else:
+            self.initialize_database()
+
+        self.ops = {
+            'create_project': (self.create_project, ['name']),
+            'delete_project': (self.delete_project, ['name']),
+            'create_task': (self.create_task, ['parent', 'name'])
+        }
+
+    def initialize_database(self):
+        logger.info('Initializing database...')
+        self.metadata = {
+            'projects': [],
+            'archived': [],
+            'create_time': int(time.time())
+        }
+        self.save_metadata()
+        os.mkdir(self.proj_dir_path)
+
     def has_project(self, name: str) -> bool:
         """Check if a project exists by name.
         :param name: Project name.
         """
-        proj_path = os.path.join(self.proj_dir_path, name)
-        return os.path.exists(proj_path) and os.path.isdir(proj_path)
+        return name in self.metadata.get('projects', [])
 
-    def create_project(self, name: str, desc: str = '') -> Project:
+    def create_project(self, name: str) -> Project:
         """Create a project.
         :param name: Project name.
         :param desc: Project description.
         """
         if self.has_project(name):
-            raise ValueError('project {} exists'.format(name))
+            raise ValueError('Project {} exists'.format(name))
+        if not validate_name(name):
+            raise ValueError('Invalid project name')
 
-        proj = Project(name, desc)
+        proj = Project(name,
+                       path=os.path.join(self.proj_dir_path, name))
         proj.create()
-        self.add_metadata('projects', name)
+        proj.add_metadata('name', name)
+        proj.add_metadata('identifier', name)
+        self.append_metadata_item('projects', name)
 
         return proj
 
@@ -351,9 +273,41 @@ class Database(object):
            shutil.rmtree(os.path.join(self.proj_dir_path, name))
            self.remove_metadata_item('projects', name)
         else:
-            raise ValueError('project {} does not exists'.format(name))
+            raise ValueError('Project {} does not exist'.format(name))
 
+    def get_project(self, name: str):
+        if self.has_project(name):
+            return Project(name, path=os.path.join(self.proj_dir_path, name))
+        else:
+            raise ValueError('Project {} does not exist'.format(name))
 
+    def get_child(self, name: str):
+        if '/' not in name:
+            if self.has_project(name):
+                return self.get_project(name)
+            else:
+                raise ValueError('Project {} does not exist')
+        else:
+            name = name.split('/')
+            current = self.get_project(name[0])
+            tasks = name[1:]
+            for task in tasks:
+                current = current.get_child(task)
+            return current
 
+    def create_task(self, parent: str, name: str):
+        parent = self.get_child(parent)
+        parent.create_child(name)
 
-
+    def api(self, op, args):
+        try:
+            if op in self.ops:
+                op_func, op_args = self.ops[op]
+                args = {arg: args[arg] for arg in op_args}
+                op_func(**args)
+                return True, 'Success'
+            else:
+                return False, 'Unknown operation: {}'.format(op)
+        except Exception as e:
+            traceback.print_exc()
+            return False, str(e)

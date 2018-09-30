e4b3a4f4d480940cf51ccba6d7a2eb004a7a4850
limteng-rpi <linying@limteng.com>
2018-09-30 18:30:54 -0400

    retrieve table type result

--- static/js/task_view.js
+++ static/js/task_view.js
@@ -17,8 +17,30 @@ function retrieve_config() {
         'data': {identifier: _identifier},
         'success': function (data) {
             var config = data.data;
+            console.log(config);
         }
     })
+}
+
+function _handle_table_result(key, val) {
+    var table = $('<li class="tv-result-li"><table></table></li>');
+    var thead = $('<thead><tr></tr></thead>');
+    $.each(val.cols, function (i, col) {
+        thead.append($('<td></td>').text(col));
+    });
+    var tbody = $('<tbody></tbody>');
+    $.each(val.data, function (i, row) {
+        var tr = $('<tr></tr>');
+        $.each(row, function (j, v) {
+            if (typeof v === 'number' && !Number.isInteger(v)) {
+                v = Number.parseFloat(v).toPrecision(2);
+            }
+            tr.append($('<td></td>').text(v));
+        });
+    });
+
+    table.append(thead, tbody);
+    $('ul#tv-result-list').append(table);
 }
 
 function retrieve_result() {
@@ -27,6 +49,19 @@ function retrieve_result() {
         'data': {identifier: _identifier},
         'success': function (data) {
             var result = data.data;
+            console.log(result);
+            $.each(result, function (rst_key, rst) {
+                var rst_type = rst.type;
+                var rst_val = rst.value;
+
+                switch (rst_type) {
+                    case 'table':
+                        _handle_table_result(rst_key, rst_val);
+                        break;
+                    default:
+                        break;
+                }
+            });
         }
-    })
+    });
 }

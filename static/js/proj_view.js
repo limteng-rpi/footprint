0222ade313c073f43162a9d3ecbbd6590c52ad72
limteng-rpi <linying@limteng.com>
2018-10-14 17:35:21 -0400

    proj name click func

--- static/js/proj_view.js
+++ static/js/proj_view.js
@@ -3,21 +3,25 @@
  * Date: Sep 30, 2018
  */
 
+// const show_archive = false;
+
 $(document).ready(function () {
     retrieve_project_list();
-});
+})
+    .on('click', '.proj-s-name', proj_title_click);
 
 function update_project_list(projs) {
     var proj_list = $('#proj-list');
     proj_list.empty();
     $.each(projs, function (i, proj) {
-        console.log(proj)
         var name = proj.name;
         var identifier = proj.identifier;
         var task_num = (typeof proj.tasks === 'undefined')? 0 : proj.tasks.length;
         var run_num = 0;
         var fail_num = 0;
         var done_num = 0;
+        var archive = proj.archive || false;
+
         if (task_num > 0) {
             $.each(proj.tasks, function (ti, task) {
                 switch (task.status) {
@@ -36,24 +40,31 @@ function update_project_list(projs) {
             });
         }
 
-        var proj_li = $('<li></li>').addClass('proj-li');
+        var proj_li = $('<li></li>')
+            .addClass('proj-li')
+            .attr('archived', archive);
         // Project information
         var proj_info = $('<div></div>').addClass('proj-info');
         var proj_summary = $('<div></div>').addClass('proj-summary');
-        proj_summary.append($('<span class="proj-s-name ellipsis"></span>').text(name));
+        proj_summary.append($('<span class="proj-s-name ellipsis"></span>')
+            .text(name)
+            .attr('identifier', identifier));
         proj_summary.append($('<span class="proj-s-task"></span>').text('Task: ' + task_num));
         proj_summary.append($('<span class="proj-s-run"></span>').html('<i class="fas fa-circle fa-sm"></i> Running: ' + run_num));
         proj_summary.append($('<span class="proj-s-fail"></span>').html('<i class="fas fa-circle fa-sm"></i> Failed: ' + fail_num));
         proj_summary.append($('<span class="proj-s-done"></span>').html('<i class="fas fa-circle fa-sm"></i> Done: ' + done_num));
 
         var proj_ops = $('<div></div>').addClass('proj_ops');
-        var proj_arch_btn = $('<button class="trans gray"><i class="far fa-eye-slash"></i> Archive</button>');
+        var proj_arch_btn = $('<button class="trans gray"><i class="far fa-eye-slash"></i> Archive</button>')
+            .attr('identifier', identifier);
         proj_ops.append(proj_arch_btn);
 
         proj_info.append(proj_summary, proj_ops);
 
         // Project tasks
-        var proj_tasks = $('<div></div>').addClass('proj-tasks');
+        var proj_tasks = $('<div></div>')
+            .addClass('proj-tasks')
+            .attr('identifier', identifier);
         var proj_tasks_ops = $('<div></div>').addClass('proj-tasks-ops');
         var proj_tasks_ops_left = $('<div></div>').addClass('proj-tasks-ops-left');
         var new_task_btn = $('<button class="outline"><i class="fas fa-plus"></i> New Task</button>')
@@ -93,4 +104,15 @@ function retrieve_project_list() {
             update_project_list(data.data);
         }
     });
+}
+
+function show_archieved_btn_click() {
+    var btn = $(this);
+    var i = btn.first('i')
+}
+
+function proj_title_click() {
+    var title = $(this);
+    var identifier = title.attr('identifier');
+    $('.proj-tasks[identifier="' + identifier + '"]').toggle();
 }
